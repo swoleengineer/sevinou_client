f81f954a2b8f6805f5099ef4c783f95060bf3911
Joram Clervius <swoleengineer@jorams-mbp.lan>
2020-05-14 02:46:39 -0700

    fixed css issues.

--- pages/[lang]/index.tsx
+++ pages/[lang]/index.tsx
@@ -7,27 +7,63 @@ import { SectionTitle } from '../../components/typography';
 
 const Home = () => {
   return (
+    <>
+    <Homehero />
     <Layout
       titleKey={'nav.home'}
       style={{
         marginTop: 56,
         position: 'relative',
-        overflow: 'auto'
       }}
     >
-      <Homehero />
+      
       <section 
         className='sv_home_content_wrapper'
       >
-        <div className='container' style={{ position: 'relative' }}>
+        <div className='container' style={{ position: 'relative', height: 150 }}>
           <HomeCta />
-          <div className='row' style={{ padding: '100px 0' }}>
+        </div>
+        <div className='container'>
+          <div className='row' >
             <div className='col-12'>
               <SectionTitle
                 title='Get Involved'
               />
             </div>
-            {['Medical Professionals', 'Supplies', 'Donate'].map(service => {
+            <div className='col-md-4'>
+              <div 
+                className='sv_homepage_service'
+                style={{
+                  backgroundImage: `url(/assets/home_engage_medical.png)`
+                }}
+              >
+                <span className='sv_hs_title'>Medical Professionals</span>
+                <span className='sv_hs_subtitle'>Collaborate with Medical Professionals in Haiti</span>
+              </div>
+            </div>
+            <div className='col-md-4'>
+              <div 
+                className='sv_homepage_service'
+                style={{
+                  backgroundImage: `url(/assets/home_engage_supplies.png)`
+                }}
+              >
+                <span className='sv_hs_title'>Supplies</span>
+                <span className='sv_hs_subtitle'>Help source/provide medical &amp; PPE</span>
+              </div>
+            </div>
+            <div className='col-md-4'>
+              <div 
+                className='sv_homepage_service'
+                style={{
+                  backgroundImage: `url(/assets/home_engage_other.png)`
+                }}
+              >
+                <span className='sv_hs_title'>Supplies</span>
+                <span className='sv_hs_subtitle'>Help source/provide medical &amp; PPE</span>
+              </div>
+            </div>
+            {/* {['Medical Professionals', 'Supplies', 'Donate'].map(service => {
               return (
                 <div className='col-md-4' key={service}>
                   <div className='sv_homepage_service'>
@@ -35,11 +71,12 @@ const Home = () => {
                   </div>
                 </div>
               );
-            })}
+            })} */}
           </div>
         </div>
       </section>
     </Layout>
+    </>
   )
 }
 
