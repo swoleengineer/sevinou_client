a4d4b634d36552d792051da6506980a295c1ace8
Joram Clervius <swoleengineer@jorams-mbp.lan>
2020-05-14 15:28:25 -0700

    added more content to page.

--- components/typography.tsx
+++ components/typography.tsx
@@ -3,10 +3,11 @@ import React, { FC } from 'react';
 interface Props {
   title: string;
   subTitle?: string;
+  border?: boolean;
 }
-export const SectionTitle: FC<Props> = ({ title, subTitle }) => {
+export const SectionTitle: FC<Props> = ({ title, subTitle, border = false }) => {
   return (
-    <div className='sv_section_title'>
+    <div className={`sv_section_title ${border ? 'sv_section_title_border' : ''}`}>
       <div className='sv_section_title_text_section'>
         <span className='sv_section_title_title'>{title}</span>
         {subTitle && (
--- pages/[lang]/index.tsx
+++ pages/[lang]/index.tsx
@@ -5,6 +5,35 @@ import Homehero from '../../components/layout/home-hero';
 import HomeCta from '../../components/layout/home-cta';
 import { SectionTitle } from '../../components/typography';
 
+interface HomeDetails {
+  title: string;
+  content: string;
+  picture: string;
+}
+const involvedWays: Array<HomeDetails> = [{
+  title: 'Licensed Medical Professionals',
+  content: `Work with Haiti's healthcare workers in their efforts to serve their local areas via telemedicine.`,
+  picture: '/assets/home_involve_medical.png'
+}, {
+  title: 'Medical Supplies & PPE',
+  content: `Help source medical supplies and PPE for Haiti's medical centers.`,
+  picture: '/assets/home_involve_supplies.png'
+}, {
+  title: 'Other Methods',
+  content: `Support families in Haiti through food, clothing, and monetary donations.`,
+  picture: '/assets/home_ed_boxes.png'
+}];
+
+const edWays: Array<HomeDetails> = [{
+  title: 'What is Coronavirus?',
+  content: `Gain a better understanding of what the virus is and how it works.`,
+  picture: '/assets/home_ed_virus.png'
+}, {
+  title: 'How Can I Stay Safe?',
+  content: `Understand proper social-distancing and self-quarantining guidelines.`,
+  picture: '/assets/home_involve_help.png'
+}];
+
 const Home = () => {
   return (
     <>
@@ -28,53 +57,71 @@ const Home = () => {
             <div className='col-12'>
               <SectionTitle
                 title='Get Involved'
+                subTitle={`Volunteer your time or efforts to help Haiti's Covid-19 recovery`}
+                border={true}
               />
             </div>
-            <div className='col-md-4'>
-              <div 
-                className='sv_homepage_service'
-                style={{
-                  backgroundImage: `url(/assets/home_engage_medical.png)`
-                }}
-              >
-                <span className='sv_hs_title'>Medical Professionals</span>
-                <span className='sv_hs_subtitle'>Collaborate with Medical Professionals in Haiti</span>
-              </div>
-            </div>
-            <div className='col-md-4'>
-              <div 
-                className='sv_homepage_service'
-                style={{
-                  backgroundImage: `url(/assets/home_engage_supplies.png)`
-                }}
-              >
-                <span className='sv_hs_title'>Supplies</span>
-                <span className='sv_hs_subtitle'>Help source/provide medical &amp; PPE</span>
-              </div>
-            </div>
-            <div className='col-md-4'>
-              <div 
-                className='sv_homepage_service'
-                style={{
-                  backgroundImage: `url(/assets/home_engage_other.png)`
-                }}
-              >
-                <span className='sv_hs_title'>Supplies</span>
-                <span className='sv_hs_subtitle'>Help source/provide medical &amp; PPE</span>
-              </div>
-            </div>
-            {/* {['Medical Professionals', 'Supplies', 'Donate'].map(service => {
+            {involvedWays.map(way => {
+              const { title, content, picture } = way;
               return (
-                <div className='col-md-4' key={service}>
+                <div
+                  className='col-md-4'
+                  key={title}
+                >
                   <div className='sv_homepage_service'>
-                    {service}
+                    <img src={picture} alt={title} />
+                    <span className='sv_hs_title'>{title}</span>
+                    <span className='sv_hs_subtitle'>{content}</span>
                   </div>
                 </div>
               );
-            })} */}
+            })}
           </div>
         </div>
+        
       </section>
+      <section
+        style={{
+          backgroundColor: 'white',
+          margin: '-50px 0 25px',
+        }}
+      >
+        <div className='sv_bg_grey'>
+        <div className='container' style={{ marginTop: 50 }}>
+          <div className='row' >
+            <div className='col-12'>
+              <SectionTitle
+                title='Get Informed'
+                subTitle={`Learn how to take of yourself, your family, and your country.`}
+                border={true}
+              />
+            </div>
+            {edWays.map(way => {
+              const { title, content, picture } = way;
+              return (
+                <div
+                  className='col-md-6'
+                  key={title}
+                >
+                  <div className='sv_homepage_ed'>
+                    <div className='sv_homepage_ed_picture'>
+                      <img src={picture} alt={title} />
+                    </div>
+                    
+                    <div className='sv_homepage_ed_content'>
+                      <span className='sv_hs_title'>{title}</span>
+                      <span className='sv_hs_subtitle'>{content}</span>
+                    </div>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        </div>
+        
+      </section>
+      
     </Layout>
     </>
   )
